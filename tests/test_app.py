c46532c764a73b31e41dc9218f1fd1d883bacd59
wojwarych <woj.warych@gmail.com>
2018-05-11 14:49:22 +0200

    playground testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,3 +2,15 @@ def test_index(app):
     with app.test_client() as client:
         resp = client.get('/')
         assert resp.status_code == 200
+
+def test_amelinium(app):
+    with app.test_client() as client:
+        resp = client.get('/amelinium')
+        assert resp.status_code == 200
+
+def test_people(app):
+    with app.test_client() as client:
+        resp = client.get('/people')
+        assert resp.status_code == 400
+        resp = client.get('/people?n=15')
+        assert resp.status_code == 200 and resp.content_type == 'application/json'
--- views/index.py
+++ views/index.py
@@ -1,7 +1,32 @@
-from flask import render_template
+from flask import render_template, jsonify, request, abort, redirect, url_for
 from . import library
+from scripts import fake_amel
 
 
 @library.route('/')
 def index():
     return render_template('index.html')
+
+
+@library.route('/amelinium', methods=['GET'])
+def amelinium():
+    return render_template('amelinium.html')
+
+
+@library.route('/people', methods=['GET'])
+def json_sandbox():
+
+    def validate_parameters(n):
+        try:
+            populate_n = int(n)
+        except (TypeError, ValueError):
+            abort(400)
+        return populate_n
+
+    if request.method == 'GET':
+        population = validate_parameters(request.args.get('n', None))
+        if population == 0:
+            return redirect(url_for('library.index'), code=302)
+
+        fake_j = fake_amel.fake_json(population)
+        return jsonify(fake_j)
